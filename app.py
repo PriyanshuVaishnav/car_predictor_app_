9220b1828cc04e54a5945a3b2f81eb5a24a07be6
PriyanshuVaishnav <priyanshuvaishnav2566@gmail.com>
2025-05-16 02:52:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import joblib
 
-# Load model
+# Load the model
 model = joblib.load('car_price_model.pkl')
 
 st.set_page_config(page_title='🚗 Car Price Predictor', layout='centered')
@@ -12,11 +12,11 @@ year = st.number_input("Year of Purchase", min_value=1990, max_value=2024, value
 present_price = st.number_input("Present Showroom Price (in Lakh)", min_value=0.0, step=0.1)
 kms_driven = st.number_input("KMs Driven", min_value=0)
 owner = st.selectbox("Number of Previous Owners", [0, 1, 2, 3])
-
 fuel_type = st.selectbox("Fuel Type", ['Petrol', 'Diesel', 'CNG'])
 seller_type = st.selectbox("Seller Type", ['Dealer', 'Individual'])
 transmission = st.selectbox("Transmission Type", ['Manual', 'Automatic'])
 
+# Encoding maps
 fuel_map = {'Petrol': 0, 'Diesel': 1, 'CNG': 2}
 seller_map = {'Dealer': 0, 'Individual': 1}
 trans_map = {'Manual': 0, 'Automatic': 1}
